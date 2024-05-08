621863baf22c23a9f99374599cd14d4dec9861fd
whm242446 <105725880+whm242446@users.noreply.github.com>
2024-05-08 09:42:37 +0800

    Add files via upload

---
+++ data_propressing/ESM_representations.py
@@ -0,0 +1,56 @@
+import torch
+import esm
+import numpy as np
+import pandas as pd
+from Bio import SeqIO
+
+from tqdm import tqdm
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+name = "/protein_complete.fasta"
+
+
+model, alphabet = esm.pretrained.esm2_t6_8M_UR50D()
+model = model.to(device)
+batch_converter = alphabet.get_batch_converter()
+model.eval()
+
+
+error_placeholders = []
+protein_data = {}
+for seq_record in tqdm(SeqIO.parse(name, "fasta"), desc="Processing Sequences"):
+    try:
+        placeholder = seq_record.id
+        seq = str(seq_record.seq)
+        placeholder = placeholder.split("|")[1]
+
+        data = [
+            ('placeholder', seq),
+        ]
+
+        batch_labels, batch_strs, batch_tokens = batch_converter(data)
+        batch_tokens = batch_tokens.to(device)
+
+        with torch.no_grad():
+            results = model(batch_tokens, repr_layers=[33], return_contacts=True)
+
+        token_representations = results["representations"][33].cpu().numpy()
+
+        features = token_representations[0][1:-1]
+
+        protein_data[placeholder] = {
+            'seq': seq,
+            'esm_features': features
+        }
+
+    except Exception as e:
+        error_placeholders.append(placeholder)
+    finally:
+        if 'batch_tokens' in locals(): 
+            del batch_tokens
+        if 'results' in locals():
+            del results
+        if 'token_representations' in locals():
+            del token_representations
+        torch.cuda.empty_cache()
---
+++ data_propressing/dssp.py
@@ -0,0 +1,102 @@
+import os
+import numpy as np
+
+from tqdm import tqdm
+
+'''
+  根据PDB文件获取DSSP文件
+'''
+
+def check_or_create_folder(folder):
+    if not os.path.exists(folder):
+        try:
+            os.makedirs(folder)
+        except OSError as error:
+            print(f"Error creating directory {folder}: {error}")
+            return False
+    return True
+
+
+pdb_folder = '/PDB'
+output_folder = '/DSSP'
+
+
+if not check_or_create_folder(pdb_folder) or not check_or_create_folder(output_folder):
+    print("Error with input or output directory.")
+else:
+    pdb_files = [f for f in os.listdir(pdb_folder) if f.endswith('.pdb')]
+    for filename in tqdm(pdb_files, desc="Processing PDB files", unit="file"):
+        pdb_path = os.path.join(pdb_folder, filename)
+        print(pdb_path)
+        output_path = os.path.join(output_folder, filename.replace('.pdb', '.dssp'))
+
+        exit_code = os.system(f'mkdssp -v {pdb_path} {output_path}')
+        if exit_code != 0:
+            print(f"Error processing file {filename}. mkdssp exited with code {exit_code}.")
+
+
+'''
+  提取DSSP特征
+'''
+
+def process_dssp(dssp_file):
+    aa_type = "ACDEFGHIKLMNPQRSTVWY"
+    SS_type = "HBEGITSC"
+    rASA_std = [115, 135, 150, 190, 210, 75, 195, 175, 200, 170,
+                185, 160, 145, 180, 225, 115, 140, 155, 255, 230]
+
+    with open(dssp_file, "r") as f:
+        lines = f.readlines()
+
+    seq = ""
+    dssp_feature = []
+
+    p = 0
+    while lines[p].strip()[0] != "#":
+        p += 1
+    for i in range(p + 1, len(lines)):
+        aa = lines[i][13]
+        if aa == "!" or aa == "*":
+            continue
+        seq += aa
+        SS = lines[i][16]
+        if SS == " ":
+            SS = "C"
+        SS_vec = np.zeros(9) # The last dim represents "Unknown" for missing residues
+        SS_vec[SS_type.find(SS)] = 1
+        PHI = float(lines[i][103:109].strip())
+        PSI = float(lines[i][109:115].strip())
+        ACC = float(lines[i][34:38].strip())
+        ASA = min(100, round(ACC / rASA_std[aa_type.find(aa)] * 100)) / 100
+        dssp_feature.append(np.concatenate((np.array([PHI, PSI, ASA]), SS_vec)))
+
+    return seq, dssp_feature
+
+
+def transform_dssp(dssp_feature):
+    dssp_feature = np.array(dssp_feature)
+    angle = dssp_feature[:,0:2]
+    ASA_SS = dssp_feature[:,2:]
+
+    radian = angle * (np.pi / 180)
+    dssp_feature = np.concatenate([np.sin(radian), np.cos(radian), ASA_SS], axis = 1)
+
+    return dssp_feature
+
+
+def main(dssp_folder, output_folder):
+    dssp_data = {}
+    for filename in os.listdir(dssp_folder):
+        if filename.endswith('.dssp'):
+            dssp_file = os.path.join(dssp_folder, filename)
+            seq, dssp_feature = process_dssp(dssp_file)
+            transformed_feature = transform_dssp(dssp_feature)
+            filename = os.path.splitext(filename)[0]
+            dssp_data[filename] = {'seq': seq, 'dssp_features': transformed_feature}
+
+    np.save(os.path.join(output_folder, 'dssp_features.npy'), dssp_data)
+
+if __name__ == '__main__':
+    dssp_folder = '/DSSP'  
+    output_folder = ''
+    main(dssp_folder, output_folder)
